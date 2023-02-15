5f1de5bc3d2944118cd837f1062def4b71a40244
Polina Che <perhaybe@gmail.com>
2023-02-15 15:09:16 +0300

    fix: uncaughtExceptionHandler call

--- backend/utils/errorHandler/uncaughtExceptionHandler.js
+++ backend/utils/errorHandler/uncaughtExceptionHandler.js
@@ -3,7 +3,7 @@ const { UnknownError } = require('./UnknownError');
 
 const myConsole = new console.Console(process.stdout, process.stderr);
 
-module.exports.uncaughtException = (err, origin) => {
+module.exports.uncaughtExceptionHandler = (err, origin) => {
   const error = new UnknownError({
     message: `${origin} ${err.name} c текстом ${err.message} не была обработана. Обратите внимание!`,
     logMessage: `${origin} ${err.name} c текстом ${err.message} не была обработана. Обратите внимание!`,
